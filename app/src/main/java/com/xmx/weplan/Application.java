a4be19bdaaa02da8730a5f02999f9fdca57bc7c2
The-_-onE <834489218@qq.com>
2016-01-27 23:31:23 +0800

    Prepare to structure frame

---
+++ app/src/main/java/com/xmx/weplan/Application.java
@@ -0,0 +1,19 @@
+package com.xmx.weplan;
+
+import com.avos.avoscloud.AVOSCloud;
+
+/**
+ * Created by The_onE on 2016/1/3.
+ */
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AVOSCloud.initialize(this, "77ic9kqs0XrcUWCGkBxxpe2C-gzGzoHsz", "3AxKNwrCCiCdkJmADgEOeplq");
+        //AVIMMessageManager.registerMessageHandler(AVIMTextMessage.class, new MessageHandler(this));
+        //AVIMMessageManager.registerMessageHandler(AVIMMessage.class, new MessageBaseHandler(this));
+        //PushService.setDefaultPushCallback(this, ReceiveMessageActivity.class);
+        //PushService.subscribe(this, "systemmessage", ReceiveMessageActivity.class);
+        //AVInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
