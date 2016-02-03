64f517f3b50ea7eb2def62ca6f4e3ee1686c79f0
The-_-onE <834489218@qq.com>
2016-02-03 22:06:57 +0800

    Could Set the Date of plan

--- app/src/main/java/com/xmx/weplan/Plan/AddPlanActivity.java
+++ app/src/main/java/com/xmx/weplan/Plan/AddPlanActivity.java
@@ -13,10 +13,33 @@ import java.util.Date;
 
 public class AddPlanActivity extends BaseTempActivity {
     SQLManager sqlManager = SQLManager.getInstance();
+    EditText yearText;
+    EditText monthText;
+    EditText dayText;
+    EditText hourText;
+    EditText minuteText;
+    EditText secondText;
+
+    final static int[] DaysOfMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     @Override
     protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_add_plan);
+
+        yearText = getViewById(R.id.year);
+        monthText = getViewById(R.id.month);
+        dayText = getViewById(R.id.day);
+        hourText = getViewById(R.id.hour);
+        minuteText = getViewById(R.id.min);
+        secondText = getViewById(R.id.sec);
+
+        Date now = new Date(System.currentTimeMillis());
+        yearText.setText("" + (now.getYear() + 1900));
+        monthText.setText("" + (now.getMonth() + 1));
+        dayText.setText("" + now.getDay());
+        hourText.setText("" + now.getHours());
+        minuteText.setText("" + now.getMinutes());
+        secondText.setText("" + now.getSeconds());
     }
 
     @Override
@@ -25,41 +48,68 @@ public class AddPlanActivity extends BaseTempActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText hourText = getViewById(R.id.hour);
-                EditText minuteText = getViewById(R.id.min);
-                EditText secondText = getViewById(R.id.sec);
                 EditText titleText = getViewById(R.id.title);
                 if (titleText.getText().toString().equals("")) {
                     showToast("请输入标题");
                     return;
-                } else if (minuteText.getText().toString().equals("")) {
-                    showToast("请输入时间");
+                } else if (yearText.getText().toString().equals("")) {
+                    showToast("请输入年份");
+                    return;
+                } else if (monthText.getText().toString().equals("")) {
+                    showToast("请输入月份");
+                    return;
+                } else if (dayText.getText().toString().equals("")) {
+                    showToast("请输入日期");
                     return;
                 } else if (hourText.getText().toString().equals("")) {
-                    showToast("请输入时间");
+                    showToast("请输入小时");
+                    return;
+                } else if (minuteText.getText().toString().equals("")) {
+                    showToast("请输入分钟");
                     return;
                 } else if (secondText.getText().toString().equals("")) {
-                    showToast("请输入时间");
+                    showToast("请输入秒钟");
                     return;
                 }
 
                 Date plan = new Date(System.currentTimeMillis());
+                int year = Integer.valueOf(yearText.getText().toString());
+                int month = Integer.valueOf(monthText.getText().toString());
+                int day = Integer.valueOf(dayText.getText().toString());
                 int hour = Integer.valueOf(hourText.getText().toString());
                 int minute = Integer.valueOf(minuteText.getText().toString());
                 int second = Integer.valueOf(secondText.getText().toString());
-                if (0 <= hour && hour <=27) {
+                if (year <= 2099) {
+                    plan.setYear(year - 1900);
+                } else {
+                    showToast("年份过大");
+                    return;
+                }
+                if (1 <= month && month <= 12) {
+                    plan.setMonth(month - 1);
+                } else {
+                    showToast("月份格式不正确");
+                    return;
+                }
+                if (1 <= day && day <= DaysOfMonth[month - 1]) {
+                    plan.setDate(day);
+                } else {
+                    showToast("日期格式不正确");
+                    return;
+                }
+                if (0 <= hour && hour <= 30) {
                     plan.setHours(hour);
                 } else {
                     showToast("小时格式不正确");
                     return;
                 }
-                if (0 <= minute && minute <=59) {
+                if (0 <= minute && minute <= 59) {
                     plan.setMinutes(minute);
                 } else {
                     showToast("分钟格式不正确");
                     return;
                 }
-                if (0 <= second && second <=59) {
+                if (0 <= second && second <= 59) {
                     plan.setSeconds(second);
 
                 } else {
@@ -67,12 +117,17 @@ public class AddPlanActivity extends BaseTempActivity {
                     return;
                 }
 
-                String title = titleText.getText().toString();
-                if (sqlManager.insertPlan(title, "", plan)) {
-                    showToast("添加成功");
-                    finish();
+                long now = System.currentTimeMillis();
+                if (plan.getTime() > now) {
+                    String title = titleText.getText().toString();
+                    if (sqlManager.insertPlan(title, "", plan)) {
+                        showToast("添加成功");
+                        finish();
+                    } else {
+                        showToast("添加失败");
+                    }
                 } else {
-                    showToast("添加失败");
+                    showToast("请输入将来的时间");
                 }
             }
         });
