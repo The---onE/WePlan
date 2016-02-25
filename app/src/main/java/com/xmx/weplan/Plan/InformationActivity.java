240e7821abfded8bea2997eff84f379d897cc94b
The-_-onE <834489218@qq.com>
2016-02-25 13:42:26 +0800

    Use BGARefreshLayout to Refresh List in InformationActivity

--- app/src/main/java/com/xmx/weplan/Plan/InformationActivity.java
+++ app/src/main/java/com/xmx/weplan/Plan/InformationActivity.java
@@ -1,17 +1,36 @@
 package com.xmx.weplan.Plan;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.xmx.weplan.ActivityBase.BaseTempActivity;
-import com.xmx.weplan.Constants;
-import com.xmx.weplan.Database.SQLManager;
+import com.xmx.weplan.Database.PlanManager;
 import com.xmx.weplan.R;
 
-public class InformationActivity extends BaseTempActivity {
+import cn.bingoogolapple.refreshlayout.BGANormalRefreshViewHolder;
+import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
+import cn.bingoogolapple.refreshlayout.BGARefreshViewHolder;
+
+public class InformationActivity extends BaseTempActivity
+        implements BGARefreshLayout.BGARefreshLayoutDelegate {
+
+    ListView planList;
+    PlanCardAdapter adapter;
+    BGARefreshLayout mRefreshLayout;
+
+    private class RefreshHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (PlanManager.getInstance().updatePlans() >= 0) {
+                adapter.changeList();
+            }
+            mRefreshLayout.endRefreshing();
+        }
+    }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
@@ -19,9 +38,11 @@ public class InformationActivity extends BaseTempActivity {
 
         setTitle("计划");
 
-        ListView planList = getViewById(R.id.list_plan_card);
-        PlanCardAdapter adapter = new PlanCardAdapter(this);
+        planList = getViewById(R.id.list_plan_card);
+        adapter = new PlanCardAdapter(this);
         planList.setAdapter(adapter);
+
+        initRefreshLayout();
 
         int id = getIntent().getIntExtra("id", -1);
         for (int i = 0; i < adapter.getCount(); ++i) {
@@ -32,11 +53,48 @@ public class InformationActivity extends BaseTempActivity {
         }
     }
 
+    private void initRefreshLayout() {
+        mRefreshLayout = getViewById(R.id.item_refresh);
+        // 为BGARefreshLayout设置代理
+        mRefreshLayout.setDelegate(this);
+        // 设置下拉刷新和上拉加载更多的风格     参数1：应用程序上下文，参数2：是否具有上拉加载更多功能
+        BGARefreshViewHolder refreshViewHolder = new BGANormalRefreshViewHolder(this, true);
+        // 设置下拉刷新和上拉加载更多的风格
+        mRefreshLayout.setRefreshViewHolder(refreshViewHolder);
+
+        // 设置正在加载更多时的文本
+        refreshViewHolder.setLoadingMoreText("正在加载");
+
+        // 为了增加下拉刷新头部和加载更多的通用性，提供了以下可选配置选项  -------------START
+        // 设置整个加载更多控件的背景颜色资源id
+        //refreshViewHolder.setLoadMoreBackgroundColorRes(loadMoreBackgroundColorRes);
+        // 设置整个加载更多控件的背景drawable资源id
+        //refreshViewHolder.setLoadMoreBackgroundDrawableRes(loadMoreBackgroundDrawableRes);
+        // 设置下拉刷新控件的背景颜色资源id
+        //refreshViewHolder.setRefreshViewBackgroundColorRes(refreshViewBackgroundColorRes);
+        // 设置下拉刷新控件的背景drawable资源id
+        //refreshViewHolder.setRefreshViewBackgroundDrawableRes(refreshViewBackgroundDrawableRes);
+        // 设置自定义头部视图（也可以不用设置）     参数1：自定义头部视图（例如广告位）， 参数2：上拉加载更多是否可用
+        //mRefreshLayout.setCustomHeaderView(mBanner, false);
+        // 可选配置  -------------END
+    }
+
     @Override
     protected void setListener() {
     }
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onBGARefreshLayoutBeginRefreshing(BGARefreshLayout bgaRefreshLayout) {
+        RefreshHandler timerHandler = new RefreshHandler();
+        timerHandler.sendEmptyMessage(0);
+    }
+
+    @Override
+    public boolean onBGARefreshLayoutBeginLoadingMore(BGARefreshLayout bgaRefreshLayout) {
+        return false;
     }
 }
